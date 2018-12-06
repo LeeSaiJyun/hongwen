5fee6ed850138c14650ae61a86372ee39baa98dd
leesaijyun <leesaijyun@gmail.com>
2018-12-06 10:10:29 +0800

    api

---
+++ application/api/controller/Reservation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use think\Db;
+use think\Loader;
+use think\Request;
+
+/**
+ * 预约咨询接口
+ * @author leesaijyun
+ */
+class Reservation extends Api
+{
+
+    /**
+     * 模型对象
+     * @var \think\Model
+     */
+    protected $model = null;
+
+    protected $noNeedLogin = [''];
+    protected $noNeedRight = [''];
+
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->model = new \app\api\model\Reservation();
+    }
+
+    /**
+     * 预约
+     * @ApiMethod    API接口请求方法: POST
+     * @param $name string 姓名
+     * @param $telephone string 手机号
+     * @param $appointedtime int 预约时间
+     */
+    public function add(Request $request)
+    {
+        $data['name'] = $request->param('name');
+        $data['telephone'] = $request->param('telephone');
+        $data['appointedtime'] = $request->param('appointedtime');
+
+        //todo    添加用户ID
+        $data['user_id'] = $this->auth->id;
+
+        $validate = validate('Reservation');
+
+        if(!$validate->check($data)){
+            $this->error($validate->getError());
+        }
+        $result = $this->model->allowField(['name','telephone','appointedtime'])->save($data);
+        $this->success($result);
+    }
+
+
+}
